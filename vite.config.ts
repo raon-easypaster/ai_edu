1ff2e7c836c04f47642dae7d4740b1bc8dcc8934
이광복 <galeb76@gmail.com>
2025-12-07 16:34:34 +0900

    feat: Update project metadata and build configuration
    Adjusts the application title and language to Korean. Updates the favicon source and dependencies in package.json. Modifies vite.config.ts to ensure the correct output directory for builds and to maintain the GitHub Pages base path. Reorders dependencies in package.json for clarity.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ai_edu/', // GitHub Pages 저장소 이름에 맞춰 기본 경로 설정
+  base: '/ai_edu/', // GitHub Pages 저장소 이름
+  build: {
+    outDir: 'dist',
+  }
 });
